e4eb5d2673c4477a61e03c8744297b6747250210
RohanKhatua <contact@rohankhatua.dev>
2023-12-26 10:51:04 +0530

    feat: depth 1 piece hungry

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,108 +1,71 @@
 ﻿using System;
+using System.Collections.Generic;
 using ChessChallenge.API;
 
 public class MyBot : IChessBot
 {
+    private Dictionary<string, float> pieceValues;
+
+    public MyBot()
+    {
+        InitializePieceValues();
+    }
+
     public Move Think(Board board, Timer timer)
     {
+        string color = board.IsWhiteToMove ? "White" : "Black";
         Move[] moves = board.GetLegalMoves();
-        string myColour = board.IsWhiteToMove ? "White" : "Black";
-        Console.WriteLine("My colour is " + myColour);
+
+        float bestScore = color == "White" ? float.MinValue : float.MaxValue;
         Move bestMove = moves[0];
-        float bestEval = 0;
+
         foreach (Move move in moves)
         {
-            Console.WriteLine("Trying move: " + move);
             board.MakeMove(move);
-
-            // If the move results in checkmate, return it immediately.
-            if (board.IsInCheckmate())
-            {
-                board.UndoMove(move);
-                return move;
-            }
-
-            float currentEval = Evaluate(board);
-            Console.WriteLine("Current eval: " + currentEval);
-            if (myColour == "White")
-            {
-                if (currentEval > bestEval)
-                {
-                    bestEval = currentEval;
-                    bestMove = move;
-                }
-            }
-            else
-            {
-                if (currentEval < bestEval)
-                {
-                    bestEval = currentEval;
-                    bestMove = move;
-                }
-            }
+            float score = Evaluate(board);
             board.UndoMove(move);
+
+            bestMove = DetermineBestMove(color, move, score, ref bestScore, bestMove);
         }
 
         return bestMove;
     }
 
-    // Positive score means white is winning, negative score means black is winning.
-    public float Evaluate(Board board)
+    private Move DetermineBestMove(string color, Move move, float score, ref float bestScore, Move currentBestMove)
     {
-        float whiteScore = 0;
-        float blackScore = 0;
-        PieceList[] allPieceLists = board.GetAllPieceLists();
-        foreach (PieceList pieceList in allPieceLists)
+        if ((color == "White" && score > bestScore) || (color == "Black" && score < bestScore))
         {
-            if (pieceList.IsWhitePieceList)
-            {
-                if (pieceList.TypeOfPieceInList == PieceType.Queen)
-                {
-                    whiteScore += 9;
-                }
-                else if (pieceList.TypeOfPieceInList == PieceType.Rook)
-                {
-                    whiteScore += 5;
-                }
-                else if (pieceList.TypeOfPieceInList == PieceType.Bishop)
-                {
-                    whiteScore += 3;
-                }
-                else if (pieceList.TypeOfPieceInList == PieceType.Knight)
-                {
-                    whiteScore += 3;
-                }
-                else if (pieceList.TypeOfPieceInList == PieceType.Pawn)
-                {
-                    whiteScore += 1;
-                }
-            }
-            else
-            {
-                if (pieceList.TypeOfPieceInList == PieceType.Queen)
-                {
-                    blackScore += 9;
-                }
-                else if (pieceList.TypeOfPieceInList == PieceType.Rook)
-                {
-                    blackScore += 5;
-                }
-                else if (pieceList.TypeOfPieceInList == PieceType.Bishop)
-                {
-                    blackScore += 3;
-                }
-                else if (pieceList.TypeOfPieceInList == PieceType.Knight)
-                {
-                    blackScore += 3;
-                }
-                else if (pieceList.TypeOfPieceInList == PieceType.Pawn)
-                {
-                    blackScore += 1;
-                }
-            }
+            bestScore = score;
+            return move;
         }
 
-        return whiteScore - blackScore;
+        return currentBestMove;
     }
 
+    private void InitializePieceValues()
+    {
+        pieceValues = new Dictionary<string, float> {
+            { "Queen", 9 },
+            { "Rook", 5 },
+            { "Bishop", 3 },
+            { "Knight", 3 },
+            { "Pawn", 1 }
+        };
+    }
+
+    public float Evaluate(Board board)
+    {
+        float score = 0;
+
+        foreach (KeyValuePair<string, float> entry in pieceValues)
+        {
+            PieceType pieceType = (PieceType)Enum.Parse(typeof(PieceType), entry.Key);
+            PieceList whitePieces = board.GetPieceList(pieceType, true);
+            PieceList blackPieces = board.GetPieceList(pieceType, false);
+
+            score += (whitePieces.Count - blackPieces.Count) * entry.Value;
+        }
+
+        return score;
+    }
 }
